e38605b5340997465b1500680001c7b6fb1ec37f
Daniel Gamboa <d.gamboaflores@gmail.com>
2026-04-01 17:04:33 -0300

    marge prod

--- produccion.sh
+++
@@ -1,2 +0,0 @@
-aws s3 sync . s3://metbus-site/ --delete
-aws cloudfront create-invalidation --distribution-id E33B8GJ78OSUMP --paths "/*"
--- staging.sh
+++
@@ -1,2 +0,0 @@
-aws s3 sync . s3://staging-metbus-site/ --delete
-aws cloudfront create-invalidation --distribution-id ERAGX2ORKGDNP --paths "/*"
